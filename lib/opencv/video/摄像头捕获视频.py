4d2be8f3af9c899923fe02c9cd4114b5e0f0d9ee
houjingru <houjingru07@gmail.com>
2020-02-06 16:41:34 +0800

    opencv-修改摄像头捕获视频

--- lib/opencv/video/摄像头捕获视频.py
+++ lib/opencv/video/摄像头捕获视频.py
@@ -9,9 +9,8 @@ while True:
     ret, frame = cap.read()
     # 颜色空间转换: BGR转化为灰度图
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.cv2.imshow('frame', frame)
     # cv2.imshow('frame', gray)
+    cv2.cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
